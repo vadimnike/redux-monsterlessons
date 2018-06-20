db2794844e8dabe9f20bae6cfb1cd0315719581b
vadim <vadym.medzhitov@thinkmobiles.com>
2018-06-20 10:48:39 +0300

    redux lesson 3

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
+
+// import './App.css';
 
 class App extends Component {
   render() {
+    console.log(this.props.testStore);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React dfadfd</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <input type="text"/>
+          <button>Add track</button>
+          <ul>
+            {this.props.testStore.map((track, index)=> {
+              return(
+                <li key={index}>{track}</li>
+              )
+            })}
+          </ul>
       </div>
     );
   }
 }
 
-export default App;
+export default connect(
+  state=>({
+    testStore: state
+  }),    //map state to props
+  dispatch=>({})
+)(App);
--- src/index.js
+++ src/index.js
@@ -1,65 +1,84 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import Header from './Header';
-// import registerServiceWorker from './registerServiceWorker';
-//
-// let menu =[
-//   {
-//     link: '/articles',
-//     label: 'Articles'
-//   },
-//   {
-//     link: '/contacts',
-//     label: 'Contacts'
-//   },
-//   {
-//     link: '/posts',
-//     label: 'Posts'
-//   }
-// ];
-//
-//
-// ReactDOM.render(<Header items={menu}/>, document.getElementById('root'));
-// registerServiceWorker();
-
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {Provider} from 'react-redux';
 import {createStore} from 'redux';
+import registerServiceWorker from './registerServiceWorker';
+
+let menu =[
+  {
+    link: '/articles',
+    label: 'Articles'
+  },
+  {
+    link: '/contacts',
+    label: 'Contacts'
+  },
+  {
+    link: '/posts',
+    label: 'Posts'
+  }
+];
+
+let initialState = [
+  'Smells likes spirit',
+  'Entry Sandman'
+];
 
 //reducer playlist
-
-let playlist=(state=[], action) => {
+let playlist=(state=initialState, action) => {
   if(action.type==='ADD_TRACK'){
     return [...state, action.payload];
   } return state
 };
 
 const store = createStore(playlist);
-const addTrackBtn = document.querySelector('.add-track');
-const list =document.querySelector('.list');
-const trackInput = document.querySelector('.track-input');
 
-store.subscribe(()=> {
-  console.log('subscribe', store.getState());
-
-  list.innerHTML='';
-  trackInput.value = '';
-  store.getState().forEach(track=> {
-    let listItem = document.createElement('li');
-    listItem.textContent = track;
-    list.appendChild(listItem)
-    // list.appendChild()
-  })
-});
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root'));
+registerServiceWorker();
 
 
-// store.dispatch({type: 'ADD_TRACK', payload: 'Smells like spirit'});
-
-
-
-
-addTrackBtn.addEventListener('click', ()=>{
-  const trackName = trackInput.value;
-  console.log('trackName', trackName);
-  store.dispatch({type: 'ADD_TRACK', payload: trackName});
-});
+// import {createStore} from 'redux';
+//
+// //reducer playlist
+//
+// let playlist=(state=[], action) => {
+//   if(action.type==='ADD_TRACK'){
+//     return [...state, action.payload];
+//   } return state
+// };
+//
+// const store = createStore(playlist);
+// const addTrackBtn = document.querySelector('.add-track');
+// const list =document.querySelector('.list');
+// const trackInput = document.querySelector('.track-input');
+//
+// store.subscribe(()=> {
+//   console.log('subscribe', store.getState());
+//
+//   list.innerHTML='';
+//   trackInput.value = '';
+//   store.getState().forEach(track=> {
+//     let listItem = document.createElement('li');
+//     listItem.textContent = track;
+//     list.appendChild(listItem)
+//     // list.appendChild()
+//   })
+// });
+//
+//
+// // store.dispatch({type: 'ADD_TRACK', payload: 'Smells like spirit'});
+//
+//
+//
+//
+// addTrackBtn.addEventListener('click', ()=>{
+//   const trackName = trackInput.value;
+//   console.log('trackName', trackName);
+//   store.dispatch({type: 'ADD_TRACK', payload: trackName});
+// });
